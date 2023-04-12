d8529d65deabe8d2578222436c2ade760f1a6107
Erel Galon <erelgalon@gmail.com>
2023-04-12 17:17:11 +0300

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 import apiClinet from "../services/api.clinet";
 import axios, { CanceledError, AxiosError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 interface FetchGamesResponse {
   count: number;
