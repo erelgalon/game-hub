7eb9d96ea703dde2730197940c90e3281f7b3dab
Erel Galon <erelgalon@gmail.com>
2023-04-12 16:20:25 +0300

    Create a costum took to fetch games

--- src/components/Gamegrid.tsx
+++ src/components/Gamegrid.tsx
@@ -1,35 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import apiClinet from '../services/api.clinet';
-import {Text} from '@chakra-ui/react'
+import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
-interface Game{
-    id: number;
-    name: string;
 
-}
-interface FetchGamesResponse{ 
-    count: number;
-    results: Game[]
-}
 
 const Gamegrid = () => {
-const [games, setGames] = useState<Game[]>([]);
-const [error, setError] = useState('');
-
-useEffect(() => {
-apiClinet.get<FetchGamesResponse>('/games')
-.then(res => setGames(res.data.results))
-.catch(err => setError(err.message));
-})
+const { games, error} = useGames();
 
   return (
     <>
-   {error && <Text>{error}</Text>}
-    <ul>
-        {games.map(game => <li key={game.id}>{game.name}</li>)}
-    </ul>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default Gamegrid
+export default Gamegrid;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript,HStack } from '@chakra-ui/react'
-import App from './App'
-import theme from './components/theme'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript, HStack } from "@chakra-ui/react";
+import App from "./App";
+import theme from "./components/theme";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
+      <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
