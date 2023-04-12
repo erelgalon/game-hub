4b9333b27b44923bbe228355bae37ff9c26e4c1e
Your Name <you@example.com>
2023-04-12 12:00:05 +0300

    Build the color mode switch

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript,HStack } from '@chakra-ui/react'
 import App from './App'
 import theme from './components/theme'
 import './index.css'
