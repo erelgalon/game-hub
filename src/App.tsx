9434914612a3186aa534fda9e8c3e3acf4a0442a
Your Name <you@example.com>
2023-04-12 11:41:12 +0300

    added dark mode

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"` // wider than 1024px
+
+    }}>
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+      <GridItem area="aside" bg="gold">
+        Aside
+      </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
